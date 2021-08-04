2a17a6d79f1172ce67e92349e05cee1e08117a43
chen_null <abc82766@gmail.com>
2021-08-04 11:18:23 +0800

    Fix missing bp_mul in `calc_max_score` function

--- src/lib.rs
+++ src/lib.rs
@@ -236,7 +236,7 @@ fn calc_max_score(
                         break;
                     }
                     if !result.contains_key(&it.character_id) {
-                        result_score += (it.score as f64 * it.skill_mul) as u32;
+                        result_score += (it.score as f64 * it.skill_mul * it.bp_mul) as u32;
                         result.insert(it.character_id, it);
                     }
                 }
